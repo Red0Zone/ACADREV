a12ba5e6ea385ea5738c9f72e97eb541cfa89a6d
Ameer Yasin <126766394+Red0Zone@users.noreply.github.com>
2025-07-03 20:49:01 +0300

    feat(config): add web.config for IIS integration and enhance database connection settings

--- config/db.js
+++ config/db.js
@@ -8,7 +8,11 @@ const db = mysql.createConnection({
   host:process.env.DB_HOST,
   user:process.env.DB_USER,
   password:process.env.DB_PASSWORD,
-  database:process.env.DB_NAME
+  database:process.env.DB_NAME,
+  
+  ssl: {
+    rejectUnauthorized: false // This is important for self-signed certificates
+  }
 });
 
 db.connect((err) => {
